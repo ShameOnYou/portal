0e911467efde133e0db3f05e7d875581d7ba669b
ShameOnYou <yannickdonders@hotmail.be>
2023-04-27 19:27:40 +0200

    commit

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,65 +1,101 @@
-import { shaderMaterial, Sparkles, Center, useTexture, useGLTF, OrbitControls } from '@react-three/drei'
-import * as THREE from 'three'
-import { useFrame, extend } from '@react-three/fiber'
-import { useRef } from 'react'
-import portalVertexShader from './shaders/portal/vertex.glsl'
-import portalFragmentShader from './shaders/portal/fragment.glsl'
+import {
+	Sparkles,
+	OrbitControls,
+	useGLTF,
+	useTexture,
+	Center,
+	Stars,
+	Sky,
+	shaderMaterial,
+} from "@react-three/drei";
+import { extend, useFrame } from "@react-three/fiber";
+import { Color } from "three";
+import { useRef } from "react";
+import { useControls } from "leva";
+
+import portalVertexShader from "./shaders/portal/vertex.glsl";
+import portalFragmentShader from "./shaders/portal/fragment.glsl";
 
 const PortalMaterial = shaderMaterial(
-    {
-        uTime: 0,
-        uColorStart: new THREE.Color('#ffffff'),
-        uColorEnd: new THREE.Color('#000000')
-    },
-    portalVertexShader,
-    portalFragmentShader
-)
+	{
+		uTime: 0,
+		uColorStart: new Color("#9e7700"),
+		uColorEnd: new Color("#f0b400"),
+	},
+	portalVertexShader,
+	portalFragmentShader
+);
 
-extend({ PortalMaterial })
+extend({ PortalMaterial });
 
-export default function Experience()
-{
-    const { nodes } = useGLTF('./model/portal.glb')
+export default function Experience() {
+	const portalMaterial = useRef();
 
-    const bakedTexture = useTexture('./model/baked.jpg')
-    bakedTexture.flipY = false
-    
-    const portalMaterial = useRef()
+	const { nodes } = useGLTF("./model/portal.glb");
+	const bakedTexture = useTexture("./model/baked.jpg");
 
-    useFrame((state, delta) =>
-    {
-        portalMaterial.current.uTime += delta
-    })
+	bakedTexture.flipY = false;
 
-    return <>
+	useFrame((state, delta) => {
+		portalMaterial.current.uTime += delta;
+	});
+	console.log(nodes.baked.geometry);
+	return (
+		<>
+			<color args={["#030202"]} attach="background" />
 
-        <OrbitControls makeDefault />
+			<OrbitControls makeDefault />
+			<Center>
+				<mesh geometry={nodes.baked.geometry}>
+					<meshBasicMaterial map={bakedTexture} />
+				</mesh>
 
-        <Center>
-            <mesh geometry={ nodes.baked.geometry }>
-                <meshBasicMaterial map={ bakedTexture } />
-            </mesh>
+				<mesh
+					geometry={nodes.poleLightA.geometry}
+					position={nodes.poleLightA.position}
+				>
+					<meshBasicMaterial color="#ffffe5" />
+				</mesh>
+				<mesh
+					geometry={nodes.poleLightB.geometry}
+					position={nodes.poleLightB.position}
+				>
+					<meshBasicMaterial color="#ffffe5" />
+				</mesh>
 
-            <mesh geometry={ nodes.poleLightA.geometry } position={ nodes.poleLightA.position }>
-                <meshBasicMaterial color="#ffffe5" />
-            </mesh>
+				<mesh
+					geometry={nodes.portalLight.geometry}
+					position={nodes.portalLight.position}
+					rotation={nodes.portalLight.rotation}
+				>
+					<portalMaterial ref={portalMaterial} />
+				</mesh>
 
-            <mesh geometry={ nodes.poleLightB.geometry } position={ nodes.poleLightB.position }>
-                <meshBasicMaterial color="#ffffe5" />
-            </mesh>
+				<Sparkles
+					size={6}
+					scale={[4, 2, 4]}
+					position-y={1}
+					speed={0.2}
+					count={40}
+					color={"#ab6227"}
+				/>
+			</Center>
 
-            <mesh geometry={ nodes.portalLight.geometry } position={ nodes.portalLight.position } rotation={ nodes.portalLight.rotation }>
-                <portalMaterial ref={ portalMaterial } />
-            </mesh>
-
-            <Sparkles
-		        size={ 6 }
-                scale={ [ 4, 2, 4 ] }
-                position-y={ 1 }
-                speed={ 0.2 }
-                count={ 40 }
-            />
-        </Center>
-
-    </>
+			<Sky
+				distance={450000}
+				sunPosition={[0, -1, 0]}
+				inclination={1}
+				azimuth={1}
+			/>
+			<Stars
+				radius={50}
+				depth={50}
+				count={5000}
+				factor={2}
+				saturation={1}
+				fade
+				speed={1}
+			/>
+		</>
+	);
 }
